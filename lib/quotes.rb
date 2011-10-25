e0b7630c68ea4b7a46cc3d9efc5aaf22df7bb215
Franklin Webber <franklin.webber@gmail.com>
2011-10-24 20:59:40 -0700

    Updated the reading sources in the exercise

--- lib/quotes.rb
+++ lib/quotes.rb
@@ -18,5 +18,9 @@ end
 # Here is where you will write the methods that are defined in the associated 
 # specifications file.
 # 
+# For more information about methods and their parameters I encourage you to 
+# read some of the following:
+# 
 # @see http://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Method_Calls
+# @see Programing Ruby, Chapter 6 
 #
