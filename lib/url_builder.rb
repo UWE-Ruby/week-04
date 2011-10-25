fd127de912433a9f2e21416f89af41888972a1cc
Franklin Webber <franklin.webber@gmail.com>
2011-10-25 11:30:35 -0700

    Exercise #url_builder

---
+++ lib/url_builder.rb
@@ -0,0 +1,12 @@
+
+
+# 
+# Here is where you will write the #url_builder method that are defined in the 
+# associated specifications file.
+# 
+# For more information about methods and their parameters I encourage you to 
+# read some of the following:
+# 
+# @see http://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Method_Calls
+# @see Programing Ruby, Chapter 6 
+#
---
+++ spec/url_builder_spec.rb
@@ -0,0 +1,43 @@
+require "spec_helper"
+
+describe "#url_builder" do
+  
+  let(:url) { "http://google.com" }
+  
+  context "when given no parameters should default to a query with 10 results" do
+    
+    it "should call the basic url" do
+      url_builder(url).should == "#{url}?results=10"
+    end
+    
+  end
+  
+  context "when given a single parameter" do
+    
+    it "appends the parameters to the url" do
+      url_builder(url,:results => 20).should == "#{url}?results=20"
+    end
+    
+  end
+  
+  context "when given multiple parametesr" do
+    
+    it "should append each parameter with an &" do
+      url_builder(url,:results => '20',:search => 'burgers').should == "#{url}?results=20&search=burgers"
+    end
+    
+  end
+  
+  context "when a parameter is nil" do
+    
+    it "should ignore the parameter" do
+      url_builder(url,:results => '20',:search => 'burgers',:sort_by => nil).should == "#{url}?results=20&search=burgers"
+    end
+    
+  end
+  
+  it "should fail" do
+    fail
+  end
+  
+end
