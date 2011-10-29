6a0c551c40f4b20ab342a8e0ce56fe25bca795a8
Franklin Webber <franklin.webber@gmail.com>
2011-10-29 16:21:50 -0700

    Adding additional url_build specs

--- spec/url_builder_spec.rb
+++ spec/url_builder_spec.rb
@@ -4,18 +4,22 @@ describe "#url_builder" do
   
   let(:url) { "http://google.com" }
   
-  context "when given no parameters should default to a query with 10 results" do
+  context "when given no parameters" do
     
-    it "should call the basic url" do
-      url_builder(url).should == "#{url}?results=10"
+    it "should generate a url with 10 results and ascending sort by defaults" do
+      url_builder(url).should == "#{url}?results=10&sort=asc"
     end
     
   end
   
   context "when given a single parameter" do
     
-    it "appends the parameters to the url" do
-      url_builder(url,:results => 20).should == "#{url}?results=20"
+    it "should override the default parameters" do
+      url_builder(url,:results => 20).should == "#{url}?results=20&sort=asc"
+    end
+    
+    it "should append the parameter to the url" do
+      url_builder(url,:search => 'burgers').should == "#{url}?results=20&sort=asc&search=burgers"
     end
     
   end
@@ -23,7 +27,7 @@ describe "#url_builder" do
   context "when given multiple parametesr" do
     
     it "should append each parameter with an &" do
-      url_builder(url,:results => '20',:search => 'burgers').should == "#{url}?results=20&search=burgers"
+      url_builder(url,:results => '20',:search => 'burgers').should == "#{url}?results=20&sort=asc&search=burgers"
     end
     
   end
@@ -31,7 +35,15 @@ describe "#url_builder" do
   context "when a parameter is nil" do
     
     it "should ignore the parameter" do
-      url_builder(url,:results => '20',:search => 'burgers',:sort_by => nil).should == "#{url}?results=20&search=burgers"
+      url_builder(url,:results => '20',:search => 'burgers',:sort_by => nil).should == "#{url}?results=20&sort=asc&search=burgers"
+    end
+    
+  end
+  
+  context "when a paramter is false" do
+    
+    it "should include the parameter" do
+      url_builder(url,:results => '20',:search => 'burgers',:safe_search => false).should == "#{url}?results=20&sort=asc&search=burgers&safe_search=false"
     end
     
   end
