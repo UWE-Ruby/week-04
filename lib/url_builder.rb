002bfecd7ec5509f8753cf4722d68932e1e184db
Franklin Webber <franklin.webber@gmail.com>
2011-10-29 16:14:49 -0700

    url_builder solution

--- lib/url_builder.rb
+++ lib/url_builder.rb
@@ -10,3 +10,11 @@
 # @see http://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Method_Calls
 # @see Programing Ruby, Chapter 6 
 #
+
+def url_builder(url,options = {})
+  options = { :results => 10 }.merge(options)
+  
+  param_string = options.reject{|k,v| v == nil }.map {|key,value| "#{key}=#{value}" }.join("&")
+  
+  "#{url}?#{param_string}"
+end
