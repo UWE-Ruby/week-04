25d9f3943f3a4a0f86e80e590cdfe70f20a08ff0
Franklin Webber <franklin.webber@gmail.com>
2011-10-29 19:27:58 -0700

    display_superheroes solution

--- lib/display_superheroes.rb
+++ lib/display_superheroes.rb
@@ -8,3 +8,10 @@
 # @see http://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Method_Calls
 # @see Programing Ruby, Chapter 6 
 #
+
+
+def display_superheroes(*heroes)
+  heroes.flatten.uniq.each do |hero|
+    puts "Hero: #{hero.name}"
+  end
+end
