6a336a6be1b97f0751ffe34c6e337806c7652dca
Franklin Webber <franklin.webber@gmail.com>
2011-10-29 19:24:16 -0700

    find_by - solve the function

--- lib/superheroes_find_by.rb
+++ lib/superheroes_find_by.rb
@@ -6,10 +6,16 @@
 # file 'spec/superheroes_find_by.rb' and uncomment those tests and change this 
 # method so that it finds superheroes that match all of the criteria.
 # 
+# def SuperHeroes.find_by(criteria)
+#   criteria.map do |field,value|
+#     find_all do |hero|
+#       hero[field] == value
+#     end
+#   end.flatten.uniq
+# end
+
 def SuperHeroes.find_by(criteria)
-  criteria.map do |field,value|
-    find_all do |hero|
-      hero[field] == value
-    end
-  end.flatten.uniq
+  criteria.inject(self) do |found,term|
+    found.find_all {|hero| hero[term.first] == term.last }
+  end
 end
--- spec/superheroes_find_by_spec.rb
+++ spec/superheroes_find_by_spec.rb
@@ -6,31 +6,31 @@ require "spec_helper"
 # 
 # Simply uncomment the examples below and allow it to guide you to the method.
 # 
-# describe "SuperHeroes#find_by" do
-#   
-#   context "when searching for one criteria" do
-#     
-#     it "should return an Array even when only character matches" do
-#       SuperHeroes.find_by(:name => 'Batman').should have(1).items
-#     end
-#     
-#     it "should return all characters that match the criteria" do
-#       SuperHeroes.find_by(:origin => 'Krypton').should have(2).items
-#     end
-#     
-#   end
-# 
-#   context "when searching for two criteria" do
-#     
-#     it "should an empty Array when no characters match" do
-#       SuperHeroes.find_by(:origin => 'Krypton', :nemesis => 'Joker').should have(0).items
-#     end
-#     
-#     it "should retun all characters that match the criteria" do
-#       SuperHeroes.find_by(:origin => 'Gotham City', :nemesis => 'Joker').should have(2).items
-#       SuperHeroes.find_by(:origin => 'Gotham City', :nick_name => 'Boy Wonder').should have(1).items
-#     end
-#     
-#   end
-#   
-# end
+describe "SuperHeroes#find_by" do
+  
+  context "when searching for one criteria" do
+    
+    it "should return an Array even when only character matches" do
+      SuperHeroes.find_by(:name => 'Batman').should have(1).items
+    end
+    
+    it "should return all characters that match the criteria" do
+      SuperHeroes.find_by(:origin => 'Krypton').should have(2).items
+    end
+    
+  end
+
+  context "when searching for two criteria" do
+    
+    it "should an empty Array when no characters match" do
+      SuperHeroes.find_by(:origin => 'Krypton', :nemesis => 'Joker').should have(0).items
+    end
+    
+    it "should retun all characters that match the criteria" do
+      SuperHeroes.find_by(:origin => 'Gotham City', :nemesis => 'Joker').should have(2).items
+      SuperHeroes.find_by(:origin => 'Gotham City', :nick_name => 'Boy Wonder').should have(1).items
+    end
+    
+  end
+  
+end
